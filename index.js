dc8a304b1db7dd5d2a464f578702177f21974dd2
Reto Brunner <reto@slightlybroken.com>
2024-12-28 09:34:41 +0100

    wip: index

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 
 process.chdir(__dirname);
 
+import fs from "fs";
+import {readFile} from "node:fs/promises";
+import semver from "semver";
 // Perform node version check before loading any other files or modules
 // Doing this check as soon as possible allows us to
 // avoid ES6 parser errors or other issues
-const pkg = require("./package.json");
+const pkg = JSON.parse(await readFile("./package.json", "utf8"));
 
-if (!require("semver").satisfies(process.version, pkg.engines.node)) {
+if (!semver.satisfies(process.version, pkg.engines.node)) {
 	/* eslint-disable no-console */
 	console.error(
 		"The Lounge requires Node.js " +
@@ -25,14 +28,11 @@ if (!require("semver").satisfies(process.version, pkg.engines.node)) {
 	process.exit(1);
 }
 
-const fs = require("fs");
-
-if (fs.existsSync("./dist/server/index.js")) {
-	require("./dist/server/index.js");
-} else {
+if (!fs.existsSync("./dist/server/index.js")) {
 	console.error(
 		"Files in ./dist/server/ not found. Please run `yarn build` before trying to run `node index.js`."
 	);
-
 	process.exit(1);
 }
+
+import "./dist/server/index.js";
